c256f6461e70731a971510b05d930edfd1e7d12a
Nick O'Connell <noconnell96@gmail.com>
2021-10-03 16:51:42 -0700

    Spawn randomly on screen

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -11,7 +11,7 @@ public class Conductor : MonoBehaviour
 
     void Start()
     {
-        spawnTimer = 0;
+        spawnTimer = 0f;
     }
 
     void Update()
@@ -19,8 +19,14 @@ public class Conductor : MonoBehaviour
         spawnTimer += Time.deltaTime;
         if (spawnTimer >= spawnInterval)
         {
-            spawnTimer = 0;
-            Instantiate(prefabs[Random.Range(0, prefabs.Length)], canvas.pixelRect.center, Quaternion.identity, canvas.transform);
+            spawnTimer = 0f;
+            if (spawnInterval > 0.5f)
+            {
+                spawnInterval -= 0.1f;
+            }
+            GameObject spawnObject = prefabs[Random.Range(0, prefabs.Length)];
+            RectTransform rectTransform = spawnObject.GetComponent<RectTransform>();
+            Instantiate(spawnObject, new Vector2(Random.Range(rectTransform.rect.width * 1.5f, canvas.pixelRect.width - rectTransform.rect.width * 1.5f), Random.Range(rectTransform.rect.height * 1.5f, canvas.pixelRect.height - rectTransform.rect.height * 1.5f)), Quaternion.identity, canvas.transform);
         }
     }
 }
